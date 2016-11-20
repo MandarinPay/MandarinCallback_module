89eca815211da72b0250e01d09a2c2bb8013f294
Xaaalera <dampirsimpl@gmail.com>
2016-11-20 23:59:37 +0300

    Update droptable.php

--- droptable.php
+++ droptable.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once('option.php');
+//require_once('option.php');
 
 
-$db = new PDO("mysql:dbname={$TableName};host=localhost", $TableUser, $TablePassword);
-$db->exec("DROP TABLE CardBinding");
-$db->exec("DROP TABLE TransactionUser");
-$result = $db->query("SHOW TABLES");
-$result = $result ->fetchAll(PDO::FETCH_COLUMN);
-print_r($result);
+//$db = new PDO("mysql:dbname={$TableName};host=localhost", $TableUser, $TablePassword);
+//$db->exec("DROP TABLE CardBinding");
+//$db->exec("DROP TABLE TransactionUser");
+//$result = $db->query("SHOW TABLES");
+//$result = $result ->fetchAll(PDO::FETCH_COLUMN);
+//print_r($result);
 
